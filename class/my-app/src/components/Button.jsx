1628bde4023968499075cab859a69652b26a0c49
mohana1212 <mkgurram83@gmail.com>
2023-07-11 15:31:23 -0700

    button HW
    HW

--- class/my-app/src/components/Button.jsx
+++ class/my-app/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = ({ onClick, children, className = "", secondary = false }) => {
         const styles = `${
-                secondary ? "bg-orange-500" : "bg-indigo-500"
+                secondary ? "bg-orange-400" : "bg-indigo-400"
         } text-white rounded-md p-1 ${className}`;
 
         return (
